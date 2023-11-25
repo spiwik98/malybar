4c443b271d40d89f971321bbf34084edfad9052e
spiwik98 <bzalewa@noreply.github.com>
2023-11-25 11:12:06 +0100

    Default view

--- malybar/urls.py
+++ malybar/urls.py
@@ -1,9 +1,8 @@
+from django.urls import path
 from django.contrib import admin
-from django.conf.urls import path, include
-
+from django.conf.urls import include
 
 urlpatterns = [
     path('', include('pizza.urls')),
     path('admin/', admin.site.urls),
 ]
-
